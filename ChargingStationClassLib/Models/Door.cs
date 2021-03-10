5991b08e222927650e13578d6dd7222f9f323011
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-03-10 20:00:09 +0100

    - Implemented initial version of classes 'IDoor' and 'Door'

--- ChargingStationClassLib/Models/Door.cs
+++ ChargingStationClassLib/Models/Door.cs
@@ -6,7 +6,53 @@ using System.Threading.Tasks;
 
 namespace ChargingStationClassLib.Models
 {
-    class Door
+    public class DoorLockEventArgs : EventArgs
     {
+        public bool IsLocked { get; set; }
+    }
+
+    public class DoorMoveEventArgs : EventArgs
+    {
+        public bool HasOpened { get; set; }
+    }
+
+    class Door : IDoor
+    {
+        public event EventHandler<DoorLockEventArgs> DoorLockEvent;
+        public event EventHandler<DoorMoveEventArgs> DoorMoveEvent;
+
+        public Door()
+        {
+            
+        }
+        public void OpenDoor()
+        {
+            OnDoorMoveEvent(new DoorMoveEventArgs { HasOpened = true });
+        }
+
+        public void CloseDoor()
+        {
+            OnDoorMoveEvent(new DoorMoveEventArgs { HasOpened = false });
+        }
+
+        public void LockDoor()
+        {
+            OnDoorLockEvent(new DoorLockEventArgs { IsLocked = true });
+        }
+
+        public void UnlockDoor()
+        {
+            OnDoorLockEvent(new DoorLockEventArgs { IsLocked = false });
+        }
+
+        protected virtual void OnDoorMoveEvent(DoorMoveEventArgs e)
+        {
+            DoorMoveEvent?.Invoke(this, e);
+        }
+
+        protected virtual void OnDoorLockEvent(DoorLockEventArgs e)
+        {
+            DoorLockEvent?.Invoke(this, e);
+        }
     }
 }
--- ChargingStationClassLib/Models/IDoor.cs
+++ ChargingStationClassLib/Models/IDoor.cs
@@ -8,8 +8,13 @@ namespace ChargingStationClassLib.Models
 {
     interface IDoor
     {
+        public event EventHandler<DoorLockEventArgs> DoorLockEvent;
+
+        public event EventHandler<DoorMoveEventArgs> DoorMoveEvent;
+
+        public void OpenDoor();
+        public void CloseDoor();
         public void LockDoor();
         public void UnlockDoor();
-
     }
 }
