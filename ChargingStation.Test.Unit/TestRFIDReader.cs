ee73e2249c092eb31502836ff953a3276c2f8a8e
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-11 08:27:36 +0100

    added test

--- ChargingStation.Test.Unit/TestRFIDReader.cs
+++ ChargingStation.Test.Unit/TestRFIDReader.cs
@@ -1,18 +1,28 @@
+using ChargingStationClassLib.Models;
 using NUnit.Framework;
 
 namespace ChargingStation.Test.Unit
 {
     public class Tests
     {
+
+        private IRFIDReader _uut;
+
         [SetUp]
         public void Setup()
         {
+            _uut = new RFIDReader();
         }
 
+        //Functional tests
         [Test]
-        public void Test1()
+        public void CardIDPropertySet_ValueIsSet()
         {
-            Assert.Pass();
+            _uut.CardID = 50;
+
+            Assert.That(_uut.CardID)(50);
         }
+
+        //Behavioral tests
     }
 }
