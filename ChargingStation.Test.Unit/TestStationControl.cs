3bce5e31c08888df3367f1d046721dd144165176
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-11 13:19:20 +0100

    add

--- ChargingStation.Test.Unit/TestStationControl.cs
+++ ChargingStation.Test.Unit/TestStationControl.cs
@@ -11,12 +11,28 @@ namespace ChargingStation.Test.Unit
 {
     class TestStationController
     {
+        private StationControl _uut;
+        private ILogFile _logfile = new LogFile();
+        private IDisplay _display = new Display();
+        private IDoor _door = new Door();
+        private IRFIDReader _rfid = new RFIDReader();
+        private IUsbCharger _uscbcharge = new UsbCharger();
+        private IChargeControl _chargecontrol = new ChargeControl(_uscbcharge);
 
         [SetUp]
         public void setup()
         {
+            _uut = new StationControl();
         }
 
-        
+
+        //Event tests
+
+
+        //Functional tests
+
+
+        //Behavioral test
+
     }
 }
