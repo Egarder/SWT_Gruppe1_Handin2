39f7ed3fe5692283851e958b7af230f2f16870c7
Egarder <61559853+Egarder@users.noreply.github.com>
2021-03-11 20:20:47 +0100

    added test rfid

--- ChargingStation.Test.Unit/TestStationControl.cs
+++ ChargingStation.Test.Unit/TestStationControl.cs
@@ -97,7 +97,6 @@ namespace ChargingStation.Test.Unit
             _display.Received(1).ShowMessage("ID Scanned and approved");
         }
 
-
         [TestCase(50)]
         public void RFIDEventhandler_stateLocked_CardIDMatch_WriteToLogIsCalled(int id)
         {
@@ -130,7 +129,15 @@ namespace ChargingStation.Test.Unit
             _logfile.WriteToLog(Arg.Any<string>(), Arg.Any<DateTime>());
         }
 
-        //===================================  **HER MANGLER AT BLIVE TESTET DET SIDSTE ELSE STATEMENT I rfidHANDLEREN****=======================================
+        [TestCase(50)]
+        public void RFIDEventhandler_StateOpened_ShowMessageCalled(int id)
+        {
+            _uut.State = StationControl.ChargingStationState.Opened;
+
+            _rfid.ScanEvent += Raise.EventWith(new ScanEventArgs { ID = id });
+
+            _display.ShowMessage("Please close the door");
+        }
 
         //Door handler tests
 
