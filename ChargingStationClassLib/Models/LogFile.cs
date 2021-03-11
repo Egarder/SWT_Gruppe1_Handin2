e1ec378c8a239c65f9f821ff0ac39874d81524e3
simonkroer <simonkroer@gmail.com>
2021-03-11 14:47:58 +0100

    Logfile...

--- ChargingStationClassLib/Models/LogFile.cs
+++ ChargingStationClassLib/Models/LogFile.cs
@@ -9,11 +9,15 @@ namespace ChargingStationClassLib.Models
 {
     public class LogFile : ILogFile
     {
+        public LogFile()
+        {
+            LogList = new List<ILog>();
+        }
         public void WriteToLog(string message,DateTime timeStamp)
         {
             LogList.Add(new Log {LogText = message,TimeStamp = timeStamp});
         }
 
-        public List<ILog> LogList { get; set; } = new List<ILog>();
+        public List<ILog> LogList { get; set; }
     }
 }
