8757b79800d94de9f3b1af5b4afda3ef8b222d54
Thomas Daugaard <thomasmdaugaard@gmail.com>
2021-03-11 13:11:35 +0100

    - added teststationcontrol

---
+++ ChargingStation.Test.Unit/TestStationControl.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ChargingStationClassLib.Models;
+using NSubstitute;
+using NUnit.Framework;
+
+namespace ChargingStation.Test.Unit
+{
+    class TestStationController
+    {
+
+        [SetUp]
+        public void setup()
+        {
+        }
+
+        }
+    }
+}
